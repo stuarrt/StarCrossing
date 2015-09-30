602fe908dd7b313cc953e92fdd7679efa748963f
Dan Grover <dmgrover1@verizon.net>
2015-09-30 15:25:14 -0400

    Created Dialogue Abstraction
    Should make future dialogue easier to add.

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -5,15 +5,17 @@ using System.Collections.Generic;
 
 public abstract class Dialogue : MonoBehaviour {
 
-	public bool cantalk;
-
-	public bool talking;
-
 	public GUIStyle testgui;
 
 	public GUIStyle diaStyle;
 
-	private static string npcname;
+	protected bool cantalk;
+	
+	protected bool talking;
+
+	protected string npcname;
+
+	protected static string textfile;
 	
 	void Update() {
 		if (talking) {
@@ -37,7 +39,25 @@ public abstract class Dialogue : MonoBehaviour {
 	}
 	
 	void OnGUI() {
-	
+		//GUI.Label (new Rect (Screen.width / 2.75f, 20, 200, 50), "Testing Dialogue Collision: " + cantalk, testgui);
+		//GUI.Label (new Rect (Screen.width / 2.75f, 70, 200, 50), "Testing Is Talking: " + talking, testgui);
+		if (cantalk) {
+			if (GUI.Button (new Rect (Screen.width - 70, Screen.height - 60, 50, 50), "Talk")) {
+				talking = true;
+				cantalk = false;
+			}
+		}
+		if (talking) {
+			GUI.Box(new Rect(10, Screen.height / 2, Screen.width - 20, Screen.height / 2 - 10), "");
+			GUI.Box(new Rect(10, Screen.height / 2, Screen.width / 5, Screen.height / 2 - 10), "");
+			GUI.Box(new Rect(10 + (Screen.width / 5), Screen.height / 2, Screen.width - (20 + (Screen.width / 5)), Screen.height / 2 - 10), "");
+			if (gameObject.tag == npcname) {
+				GUI.Label(new Rect(15 + (Screen.width / 5), Screen.height / 2, Screen.width - (20 + (Screen.width / 5)), Screen.height / 2 - 60), textfile, diaStyle);
+				if (GUI.Button (new Rect (Screen.width - 120, Screen.height - 60, 100, 50), "Next")) {
+					talking = false;
+				}
+			}
+		}
 	}
 
 }
--- Assets/Scripts/NPCDialogue.cs
+++ Assets/Scripts/NPCDialogue.cs
@@ -5,27 +5,9 @@ using System.Collections.Generic;
 
 public class NPCDialogue : Dialogue {
 
-	private static string npcname = File.ReadAllText("Assets/npcname.txt");
-	
-	void OnGUI() {
-		GUI.Label (new Rect (Screen.width / 2.75f, 20, 200, 50), "Testing Dialogue Collision: " + cantalk, testgui);
-		GUI.Label (new Rect (Screen.width / 2.75f, 70, 200, 50), "Testing Is Talking: " + talking, testgui);
-		if (cantalk) {
-			if (GUI.Button (new Rect (Screen.width - 70, Screen.height - 60, 50, 50), "Talk")) {
-				talking = true;
-				cantalk = false;
-			}
-		}
-		if (talking) {
-			GUI.Box(new Rect(10, Screen.height / 2, Screen.width - 20, Screen.height / 2 - 10), "");
-			GUI.Box(new Rect(10, Screen.height / 2, Screen.width / 5, Screen.height / 2 - 10), "");
-			GUI.Box(new Rect(10 + (Screen.width / 5), Screen.height / 2, Screen.width - (20 + (Screen.width / 5)), Screen.height / 2 - 10), "");
-			if (gameObject.tag == "NPCName") {
-				GUI.Label(new Rect(15 + (Screen.width / 5), Screen.height / 2, Screen.width - (20 + (Screen.width / 5)), Screen.height / 2 - 60), npcname, diaStyle);
-				if (GUI.Button (new Rect (Screen.width - 120, Screen.height - 60, 100, 50), "Next")) {
-					talking = false;
-				}
-			}
-		}
+	void Start() {
+		npcname = "NPCName";
+		textfile = File.ReadAllText("Assets/npcname.txt");
 	}
+
 }
