c4964baa6f3a6771e01f496bb88b27897404bcbe
stuarrt <ramgolam.s@husky.neu.edu>
2015-09-30 15:08:44 -0400

    Actual Initial Commit
    Committing Dan's initial Unity project code

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class Dialogue : MonoBehaviour {
+
+	public bool cantalk;
+
+	public bool talking;
+
+	public GUIStyle testgui;
+
+	public GUIStyle diaStyle;
+
+	private static string npcname;
+	
+	void Update() {
+		if (talking) {
+			Time.timeScale = 0;
+		}
+		if (!talking) {
+			Time.timeScale = 1;
+		}
+	}
+
+	void OnTriggerEnter(Collider npc) {
+		if (npc.gameObject.tag == "Player" && !talking) {
+			cantalk = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider npc) {
+		if (npc.gameObject.tag == "Player") {
+			cantalk = false;
+		}
+	}
+	
+	void OnGUI() {
+	
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeScale > 0) {
+			if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+				transform.position = new Vector3 (transform.position.x - 0.02f, transform.position.y, transform.position.z);
+			}
+			if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+				transform.position = new Vector3 (transform.position.x + 0.02f, transform.position.y, transform.position.z);
+			}
+			if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
+				transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 0.02f);
+			}
+			if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) {
+				transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - 0.02f);
+			}
+			//Jumping mechanic
+			//Currently player can hold down and will fly (as if having a jet pack
+			if (Input.GetKey (KeyCode.Space)) {
+				transform.position = new Vector3 (transform.position.x, transform.position.y + 0.08f, transform.position.z);
+			}
+		}
+	}
+
+}
