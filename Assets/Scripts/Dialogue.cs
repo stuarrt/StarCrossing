6fe9701bb89c709717bffb4c3cb7f7f90034c750
Dan Grover <dmgrover1@verizon.net>
2015-10-05 17:07:03 -0400

    Updated Dialogue System - Events/Quests
    NPC now gives new dialogue if the player acquires the fossil. This is a
    place-holder for future events/quests

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -23,13 +23,13 @@ public abstract class Dialogue : MonoBehaviour {
 
 	protected string[] responses = new string[1]{""};
 
-	protected string[] selection = new string[1]{"Send a text"};
+	protected string[] selection = new string[1]{"Select Response"};
 
 	protected bool[] showmenu = new bool[1];
 
 	protected int page;
-	
-	void Update() {
+
+	public virtual void Update() {
 		if (talking) {
 			Time.timeScale = 0;
 		}
@@ -53,6 +53,7 @@ public abstract class Dialogue : MonoBehaviour {
 	//Creates a drop-down menu for the soil type if it is toggled on
 	void dropMenu(float x, float y, int i, float sizeX) {
 		//Determines whether to open the drop-down part of the menu
+		if (page == 1) {
 		if (GUI.Button (new Rect (x, y, sizeX, 30), selection [i])) {
 			if (!showmenu [i]) {
 				showmenu [i] = true;
@@ -61,7 +62,6 @@ public abstract class Dialogue : MonoBehaviour {
 			}
 		}
 		if (showmenu [i]) {
-			if (page == 1) {
 				if (GUI.Button (new Rect (x, y - 30, sizeX, 30), responses[i+2])) {
 					selection [i] = responses[i+2];
 					showmenu [i] = false;
@@ -77,36 +77,40 @@ public abstract class Dialogue : MonoBehaviour {
 			}
 		}
 	}
-	
-	void OnGUI() {
-		//GUI.Label (new Rect (Screen.width / 2.75f, 20, 200, 50), "Lines Length: " + (lines.Length - 1), testgui);
-		//GUI.Label (new Rect (Screen.width / 2.75f, 70, 200, 50), "Page Length: " + page, testgui);
-		if (cantalk) {
-			if (GUI.Button (new Rect (Screen.width - 70, Screen.height - 60, 50, 50), "Talk")) {
-				talking = true;
-				cantalk = false;
-			}
-		}
+
+	public void conversation(string[] l) {
 		if (talking) {
 			GUI.Box(new Rect(10, Screen.height / 2, Screen.width - 20, Screen.height / 2 - 10), "");
 			GUI.Box(new Rect(10, Screen.height / 2, Screen.width / 5, Screen.height / 2 - 10), "");
 			GUI.Box(new Rect(10 + (Screen.width / 5), Screen.height / 2, Screen.width - (20 + (Screen.width / 5)), Screen.height / 2 - 10), "");
 			if (gameObject.tag == npcname) {
-				dropMenu(Screen.width - 320, Screen.height - 60, 0, 100);
-				if (page < (lines.Length - 1)) {
-					GUI.Label(new Rect(15 + (Screen.width / 5), Screen.height / 2, Screen.width - (20 + (Screen.width / 5)), Screen.height / 2 - 60), lines[page], diaStyle);
+				dropMenu(Screen.width - 320, Screen.height - 60, 0, 150);
+				if (page < (l.Length - 1)) {
+					GUI.Label(new Rect(15 + (Screen.width / 5), Screen.height / 2, Screen.width - (20 + (Screen.width / 5)), Screen.height / 2 - 60), l[page], diaStyle);
 					if (GUI.Button (new Rect (Screen.width - 120, Screen.height - 60, 100, 50), "Next")) {
 						page += 1;
 					}
 				}
-				if (page == (lines.Length - 1)) {
-					GUI.Label(new Rect(15 + (Screen.width / 5), Screen.height / 2, Screen.width - (20 + (Screen.width / 5)), Screen.height / 2 - 60), lines[page], diaStyle);
+				if (page == (l.Length - 1)) {
+					GUI.Label(new Rect(15 + (Screen.width / 5), Screen.height / 2, Screen.width - (20 + (Screen.width / 5)), Screen.height / 2 - 60), l[page], diaStyle);
 					if (GUI.Button (new Rect (Screen.width - 120, Screen.height - 60, 100, 50), "Goodbye")) {
 						talking = false;
 						cantalk = true;
 						page = 0;
 					}
 				}
+			}
+		}
+	}
+
+	public virtual void OnGUI() {
+		//GUI.Label (new Rect (Screen.width / 2.75f, 20, 200, 50), "Lines Length: " + (lines.Length - 1), testgui);
+		//GUI.Label (new Rect (Screen.width / 2.75f, 70, 200, 50), "Page Length: " + page, testgui);
+		GUI.Label (new Rect (Screen.width / 2.75f, 70, 200, 50), "Obtained Fossil: " + QuestList.quests[0].completed, testgui);
+		if (cantalk) {
+			if (GUI.Button (new Rect (Screen.width - 70, Screen.height - 60, 50, 50), "Talk")) {
+				talking = true;
+				cantalk = false;
 			}
 		}
 	}
--- Assets/Scripts/NPCDialogue.cs
+++ Assets/Scripts/NPCDialogue.cs
@@ -5,12 +5,26 @@ using System.Collections.Generic;
 
 public class NPCDialogue : Dialogue {
 
+	private static string textfile2;
+	private string[] lines2;
+
 	void Start() {
 		npcname = "NPCName";
-		textfile = File.ReadAllText("Assets/npcname.txt");
-		responsefile = File.ReadAllText("Assets/responses.txt");
+		textfile = File.ReadAllText ("Assets/npcname.txt");
+		responsefile = File.ReadAllText ("Assets/responses.txt");
+		textfile2 = File.ReadAllText ("Assets/npcname2.txt");
 		lines = textfile.Split('\n');
+		lines2 = textfile2.Split('\n');
 		responses = responsefile.Split('\n');
 	}
 
+	public override void OnGUI() {
+		base.OnGUI ();
+		if (!QuestList.quests [0].completed) {
+			conversation(lines);
+		}
+		if (QuestList.quests [0].completed) {
+			conversation(lines2);
+		}
+	}
 }
