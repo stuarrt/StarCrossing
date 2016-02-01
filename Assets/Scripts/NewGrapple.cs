3d34ab19d6e9c83b6f5e21e450763e9bff04cf28
Dan Grover <dmgrover1@verizon.net>
2016-02-01 12:34:35 -0500

    Updated Colliders
    Also has grapple hook testing

---
+++ Assets/Scripts/NewGrapple.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGrapple : MonoBehaviour {
+
+	public Rigidbody grapplePrefab;
+	SpringJoint joint;
+	bool grappling;
+
+	// Use this for initialization
+	void Start () {
+		joint = GetComponent<SpringJoint> ();
+		grappling = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject grapplepoint = GameObject.FindGameObjectWithTag ("GrapplePoint");
+		float a = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+		float b = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+		float c = transform.position.z;
+		if (Input.GetKeyDown (KeyCode.G)) {
+			grappling = !grappling;
+			Camera.main.orthographic = !Camera.main.orthographic;
+			if (grapplepoint != null) {
+				//grapplepoint.SetActive(grappling);
+				joint.breakForce = 0;
+			}
+		}
+		if (Input.GetMouseButtonDown (0) && grappling) {
+			joint.breakForce = Mathf.Infinity;
+			if (grapplepoint != null) {
+				DestroyObject(grapplepoint);
+			}
+			Instantiate(grapplePrefab, new Vector3(a, b, c + 10), Quaternion.identity);
+			//new Vector3(grapplepoint.transform.position.x, grapplepoint.transform.position.y, grapplepoint.transform.position.z)
+			joint.connectedAnchor = new Vector3((5 * a) - 20, (5 * b) - 20, c + 10);
+			//Debug.Log("World Space: " + transform.TransformPoint(new Vector3(a, b, c + 10)));
+			//Debug.Log("Connected Anchor: " + joint.connectedAnchor);
+		}
+	}
+}
---
+++ Assets/Scripts/TestPlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+			transform.position = new Vector3(transform.position.x + 0.05f, transform.position.y, transform.position.z);
+		}
+		if (Input.GetKey (KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position = new Vector3(transform.position.x - 0.05f, transform.position.y, transform.position.z);
+		}
+	}
+}
